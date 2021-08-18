2cc840f2e2b285fdce8abff56076da8c009e91e9
s20020 <s20020@std.it-college.ac.jp>
2021-08-18 16:43:35 +0900

    コミット確認

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
