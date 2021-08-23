b7d61e8f2c51875bb3845db7d5b5acc9bbbb4520
s20020 <s20020@std.it-college.ac.jp>
2021-08-23 23:11:53 +0900

    最初と最後の画面、そして、全部のリスト化はOK.

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,14 +1,57 @@
 package com.example.quiz
 
+import android.content.Intent
+import android.content.res.AssetManager
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.quiz.databinding.ActivityMainBinding
+import kotlinx.serialization.descriptors.StructureKind
+import java.io.InputStream
+import java.nio.file.Paths
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+
+        binding.StartButton.setOnClickListener { onChange(it) }
+
+        val assetManager = applicationContext.assets.open( "quiz.csv").bufferedReader()
+
+        var a = assetManager.use() {
+            it.readText()
+        }
+        println(assetManager)
+
+        var b = a.split(",")
+        println(b)
+        println(b.size)
+
+        val array = Array(10, {arrayOfNulls<Int>(10)})
+
+        var c =array.toList()
+
+
+        binding.testText.text = b[8]
+        }
+
+    fun onChange (view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
     }
+
+
+
 }
+
+
+
+
+
+
+
